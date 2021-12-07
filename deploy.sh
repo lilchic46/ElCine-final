14add521850cb212616aa639522eee464fc85045
lilchic46 <77950709+lilchic46@users.noreply.github.com>
2021-12-07 11:13:28 +0700

    final

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Step 1: Push the source up server ..."
-# copy multi file from local to remote
-
-#scp -r app/* truongtx@139.59.106.245:/home/truongtx/wefunder/app
-rsync -rav -e ssh --exclude=*/__pycache__ --exclude=*/migrations --exclude=env --exclude=docs \
-  ./* truongtx@139.59.106.245:/home/truongtx/elcine/
-
